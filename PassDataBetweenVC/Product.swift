43b205ef3ac11ed4a11113e7d5cf3316ee41b1e6
gulzade <gulzadekrts@gmail.com>
2022-09-11 07:30:10 +0300

    product class created

---
+++ PassDataBetweenVC/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  PassDataBetweenVC
+//
+//  Created by Gülzade Karataş on 11.09.2022.
+//
+
+import Foundation
+
+class Product {
+    
+    var productName:String?
+    var productWeight:String?
+    
+    init(productName: String, productWeight: String) {
+        self.productName = productName
+        self.productWeight = productWeight
+    }
+}
